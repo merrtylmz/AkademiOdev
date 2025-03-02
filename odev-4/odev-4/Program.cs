756e265a932f712e56048d6103d15b9f911c05a0
Mert Yılmaz <148626456+merrtylmz@users.noreply.github.com>
2025-03-02 13:51:22 +0300

    Add files via upload

---
+++ odev-4/odev-4/Program.cs
@@ -0,0 +1,71 @@
+﻿using sınıf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            user kullanıucı = new user();
+            kullanıucı.AdSoyad = "Mert YILMAZ";
+            kullanıucı.Yaş = 23;
+            kullanıucı.Email = "mertyilmaz5702@gmail.com";
+            kullanıucı.BilgiGoster();
+        }
+    }
+}
+namespace sınıf   
+{
+    class user 
+    {
+        private string adSoyad;
+        private int yas;
+        public string Email{ get; set; }
+        public string AdSoyad
+        {
+            get { return adSoyad; }
+            set { adSoyad = value; }
+        }
+
+        public int Yaş
+        {
+            get { return yas; }
+            set { yas= value; }
+        }
+        public void BilgiGoster()
+        {
+            Console.WriteLine("Ad Soyad: " + adSoyad);
+            Console.WriteLine("Yaş: " + yas);
+            Console.WriteLine("Email: " + Email);
+        }
+    }
+
+
+    /*1. Araştırma Kısmı:
+        C# dilinde kullanılan erişim belirleyiciler (public, private, protected, internal) nelerdir? Her birini gerçek hayattan bir örnekle açıklayın.
+        Erişim belirleyicilerin nesne yönelimli programlamadaki(OOP) rolü nedir?
+
+         C# dilinde kullanılan erişim belirleyiciler:
+        1. public: Herhangi bir yerden erişilebilir. 
+        2. private: Sadece tanımlandığı sınıf içerisinden erişilebilir.
+        3. protected: Sadece tanımlandığı sınıf içerisinden ve bu sınıftan türetilen sınıflardan erişilebilir.
+        4. internal: Sadece tanımlandığı proje içerisinden erişilebilir.
+    
+   Gerçek hayattan örnek:
+        1. public:  Bir kütüphanede bulunan kitapların içeriği herkes tarafından görülebilir.
+        2. private: Bir kütüphanede bulunan kitapların içeriği sadece kütüphane çalışanları tarafından görülebilir.
+        3. protected: Bir kütüphanede bulunan kitapların içeriği sadece kütüphane çalışanları ve bu çalışanlardan türetilen kişiler tarafından görülebilir.
+        4. internal: Bir kütüphanede bulunan kitapların içeriği sadece o kütüphanede çalışan kişiler tarafından görülebilir.
+    
+   Erişim belirleyicilerin nesne yönelimli programlamadaki(OOP) rolü:
+        Erişim belirleyiciler, nesne yönelimli programlamada sınıfların ve nesnelerin erişimini kontrol etmek için kullanılır. 
+        Bu sayede sınıfların ve nesnelerin erişimini kontrol ederek, programın daha güvenli ve düzenli olmasını sağlar.
+    */
+
+
+}
---
+++ odev-4/odev-4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("odev-4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("odev-4")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("507efa1b-203b-407c-8714-9e3709f2be3a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
