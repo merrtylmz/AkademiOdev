4bc1501e7f175813635bb51c73a0c84d3cdbcacf
Mert Yılmaz <148626456+merrtylmz@users.noreply.github.com>
2025-03-06 04:01:37 +0300

    Add files via upload

---
+++ odev4-DizilerveDonguler/odev4-DizilerveDonguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev4_DizilerveDonguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç adet sayı gireceksiniz?");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+            int[] sayilar = new int[adet];
+            for (int i = 0; i < adet; i++)
+            {
+                Console.WriteLine("{0}. sayıyı giriniz:", i + 1);
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+            Console.WriteLine("Ortalama: {0}", sayilar.Sum()/adet);
+
+        }
+    }
+}
