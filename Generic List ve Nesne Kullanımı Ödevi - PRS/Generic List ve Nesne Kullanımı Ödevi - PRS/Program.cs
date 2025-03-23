52357b0b5a69ea901ad6b3fe06c32e669b7a3433
Mert Yılmaz <148626456+merrtylmz@users.noreply.github.com>
2025-03-24 01:49:15 +0300

    Add files via upload

---
+++ Generic List ve Nesne Kullanımı Ödevi - PRS/Generic List ve Nesne Kullanımı Ödevi - PRS/Program.cs
@@ -0,0 +1,35 @@
+﻿using Generic_List_ve_Nesne_Kullanımı_Ödevi___PRS.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_List_ve_Nesne_Kullanımı_Ödevi___PRS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<car> araba = new List<car>();
+
+            araba.Add(new car("Bmw", "X5", 9.0, 450));
+            araba.Add(new car("Toyota", "Corolla", 6.5, 325));
+            araba.Add(new car("Mercedes", "C200", 8.2, 410));
+
+
+            foreach (car car in araba)
+            {
+                double yakıtHesabı = car.hesapla();
+                Console.WriteLine($"Marka: {car.Marka}, Model: {car.Model}, 100 km'de Yakıtı Yaktı: {car.FuelConsumption} litre, Toplam Yakıt Tüketimi: {yakıtHesabı} litre");
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Generic List ve Nesne Kullanımı Ödevi - PRS/Generic List ve Nesne Kullanımı Ödevi - PRS/classes/car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Generic_List_ve_Nesne_Kullanımı_Ödevi___PRS.classes
+{
+    internal class car
+    {
+        public string Marka;
+        public string Model;
+        public double FuelConsumption;
+        public double TotalDistance;
+
+        public car(string marka, string model, double Fuelconsumption, double Totaldistance)
+        {
+            Marka = marka;
+            Model = model;
+            FuelConsumption = Fuelconsumption;
+            TotalDistance = Totaldistance;
+        }
+
+        public double hesapla()
+        {
+            return (TotalDistance / 100) * FuelConsumption;
+        }
+
+        
+
+    }
+}
